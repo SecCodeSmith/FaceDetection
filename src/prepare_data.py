9bfa5b5b8024eef1a0ae3d0f920c46467de1470d
SecCodeSmith <admin@seccodesmith.pl>
2025-03-05 23:00:58 +0100

    Save processed data to bin file

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,17 +1,11 @@
-import concurrent
-import pickle
 from src import *
+import pickle
 import pandas as pd
 from skimage.io import imread
 from skimage.transform import resize
+from sklearn.multioutput import MultiOutputRegressor
 
 from src.feture import HOGGenerator
-
-def save_to_bin(batch_count):
-    file_name = f"tmp/features_batch_{batch_count}.bin"
-    with open(file_name, "wb") as f:
-        pickle.dump((hog_features, face_positions), f)
-    print(f"Saved batch {batch_count} to {file_name}")
 
 
 dataset_path = "DataSets/"
@@ -19,13 +13,21 @@ image_path = "img_align_celeba"
 csv_path = "list_bbox_celeba.txt"
 
 df = pd.read_csv(dataset_path + csv_path, skiprows=1, header=0, sep="\s+")
+
 hog_features = []
 face_positions = []
 
 img_size = (256, 256)
-
-batch_size=5000
+batch_size = 5000
 batch_count = 0
+
+def save_to_bin(batch_count):
+    """ Save processed batch to binary file """
+    os.makedirs("processed_data", exist_ok=True)  # Ensure the directory exists
+    file_name = f"processed_data/features_batch_{batch_count}.bin"
+    with open(file_name, "wb") as f:
+        pickle.dump((hog_features, face_positions), f)
+    print(f"Saved batch {batch_count} to {file_name}")
 
 features = HOGGenerator()
 
@@ -51,5 +53,12 @@ for index, row in df.iterrows():
     hog_features.append(feature_vector)
     face_positions.append([x_new, y_new, w_new, h_new])
 
-    if (index % 1000) == 0:
-        print(index)
+    if len(hog_features) >= batch_size:
+        save_to_bin(batch_count)
+        batch_count += 1
+        hog_features.clear()
+        face_positions.clear()
+
+# Save any remaining data
+if hog_features:
+    save_to_bin(batch_count)

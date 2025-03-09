ae03bf70d9c01cb1c8512a91dc612e65532a9e11
SecCodeSmith <admin@seccodesmith.pl>
2025-03-09 18:01:33 +0100

    Dataprocesing to class

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,11 @@ from skimage.feature import hog
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 import os
+
+PREPARE_DATA_FOLDER="processed_data"
+DATASET_FOLDER="DataSets"
+
+IMAGES_PATH=f"{DATASET_FOLDER}/img_align_celeba"
+CSV_PATH =  f"{DATASET_FOLDER}/list_bbox_celeba.txt"
+
+
--- src/prepare_data.py
+++ src/prepare_data.py
@@ -3,61 +3,62 @@ import pickle
 import pandas as pd
 from skimage.io import imread
 from skimage.transform import resize
-from sklearn.multioutput import MultiOutputRegressor
-
 from src.feture import HOGGenerator
 
 
-dataset_path = "DataSets/"
-image_path = "img_align_celeba"
-csv_path = "list_bbox_celeba.txt"
+class PrepareData:
+    def __init__(self):
+        self.df = pd.read_csv(CSV_PATH, skiprows=1, header=0, sep="\s+")
+        self.hog_features = []
+        self.face_positions = []
 
-df = pd.read_csv(dataset_path + csv_path, skiprows=1, header=0, sep="\s+")
+        self.img_size = (128, 128)
+        self.batch_size = 5000
+        self.batch_count = 0
 
-hog_features = []
-face_positions = []
+        self.features = HOGGenerator(win_size=self.img_size)
 
-img_size = (128, 128)
-batch_size = 5000
-batch_count = 0
+    def save_to_bin(selfs):
+        """ Save processed batch to binary file """
+        os.makedirs(PREPARE_DATA_FOLDER, exist_ok=True)  # Ensure the directory exists
+        file_name = f"{PREPARE_DATA_FOLDER}/features_batch_{selfs.batch_count}.bin"
+        with open(file_name, "wb") as f:
+            pickle.dump((selfs.hog_features, selfs.face_positions), f)
+        print(f"Saved batch {selfs.batch_count} to {file_name}")
+        selfs.batch_count += 1
 
-def save_to_bin(batch_count):
-    """ Save processed batch to binary file """
-    os.makedirs("processed_data", exist_ok=True)  # Ensure the directory exists
-    file_name = f"processed_data/features_batch_{batch_count}.bin"
-    with open(file_name, "wb") as f:
-        pickle.dump((hog_features, face_positions), f)
-    print(f"Saved batch {batch_count} to {file_name}")
+    def prepare(self, row):
+        img_path = os.path.join(IMAGES_PATH, row["image_id"])
+        img = imread(img_path, as_gray=True)
 
-features = HOGGenerator(win_size=img_size)
+        H_orig, W_orig = img.shape
+        x, y, w, h = row["x_1"], row["y_1"], row["width"], row["height"]
 
-for index, row in df.iterrows():
-    img_path = os.path.join(dataset_path, image_path, row["image_id"])
-    img = imread(img_path, as_gray=True)
+        img_resized = resize(img, self.img_size)
 
-    H_orig, W_orig = img.shape
-    x, y, w, h = row["x_1"], row["y_1"], row["width"], row["height"]
+        scale_x =  self.img_size[1] / W_orig
+        scale_y =  self.img_size[0] / H_orig
 
-    img_resized = resize(img, img_size)
+        x_new = int(x * scale_x)
+        y_new = int(y * scale_y)
+        w_new = int(w * scale_x)
+        h_new = int(h * scale_y)
 
-    scale_x = img_size[1] / W_orig
-    scale_y = img_size[0] / H_orig
+        feature_vector =  self.features.generate_hog(img_resized)
 
-    x_new = int(x * scale_x)
-    y_new = int(y * scale_y)
-    w_new = int(w * scale_x)
-    h_new = int(h * scale_y)
+        return feature_vector, x_new, y_new, w_new, h_new
 
-    feature_vector = features.generate_hog(img_resized)
+    def process(self):
+        for index, row in df.iterrows():
+            feature_vector, x_new, y_new, w_new, h_new = self.prepare(row)
 
-    hog_features.append(feature_vector)
-    face_positions.append([x_new, y_new, w_new, h_new])
+            self.hog_features.append(feature_vector)
+            self.face_positions.append([x_new, y_new, w_new, h_new])
 
-    if len(hog_features) >= batch_size:
-        save_to_bin(batch_count)
-        batch_count += 1
-        hog_features.clear()
-        face_positions.clear()
+            if len(self.hog_features) >= self.batch_size:
+                self.save_to_bin()
+                self.hog_features.clear()
+                self.face_positions.clear()
 
-if hog_features:
-    save_to_bin(batch_count)
+        if self.hog_features:
+            self.save_to_bin()
